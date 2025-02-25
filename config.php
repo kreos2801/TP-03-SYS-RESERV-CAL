78ca8ad497b7839852dab33ac46be07a82b258e7
Gaouditz <145690838+kreos2801@users.noreply.github.com>
2025-02-25 08:58:55 +0100

    Update config.php

--- config.php
+++ config.php
@@ -11,9 +11,9 @@ try {
     die("Erreur de connexion : " . $e->getMessage());
 }
 define("SMTP_HOST", "smtp.gmail.com"); 
-define("SMTP_USER", "yanngaouditz@gmail.com");
-define("SMTP_PASS", "kvjp lswg qzan fqca");
+define("SMTP_USER", "");
+define("SMTP_PASS", "");
 define("SMTP_PORT", 587); 
-define("SMTP_FROM", "yanngaouditz@gmail.com");
+define("SMTP_FROM", "");
 
 ?>
